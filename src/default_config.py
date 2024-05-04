c995f15159f1c0ef5b27d3eeef93b2a73b7fe3bf
sborenst <shachar.borenstein@gmail.com>
2024-05-03 21:43:51 -0400

    small refactoring started

--- src/default_config.py
+++ src/default_config.py
@@ -7,6 +7,29 @@ MODEL = 'mistral'
 BASE_URL='http://localhost:11434'
 # Streamlit Headers
 PAGE_TITLE="OpenChat"
+SIDE_BAR_TEXT="""
+        ## GitHub Repository
+
+        [![view source code ](https://img.shields.io/badge/GitHub%20Repository-gray?logo=github)](https://github.com/rhpds/OpenChatBot.git)
+
+        ## Disclaimer
+
+        **Internal use only - Experimental**
+        * This Chatbot lets you experiment with different prompts using a local LLM
+        * Interactions with Bot are monitoried, keep it work related and not private information
+        * Do not share sensitive data with the bot
+        * This bot will come and go as it is still in early development stages
+
+        ## Bot Behavior
+        * Bots and LLMs speak words that make sense, but are not necessarily true.
+        * Bot currently has no access to any company internal knowledge base
+
+        ## Example prompts: 
+        * What are the top concerns for a network engineer in an insurance company?
+        * You are a marketing content writer, help me make the following text more oriented for a seller to help them pitch this information to their customer. (Copy paste your text)
+
+        """
+
 
 # STREAMLIT_PAGE_TITLE = 'Ragnarok: Chat with **your** Data'
 # STREAMLIT_BOT_TITLE = 'AAP2 Containerized Lab Bot :female-teacher:'
--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from libs.chatbots import ContextChatbot
 import default_config as cfg
 import streamlit as st
-st.set_page_config(
-    page_title="OpenChat",
-) 
-st.header("OpenChat")
+
+def setup_streamlit(sidebar_title="sidebar title",sidebar_text="sidebar text"):
+        """
+        Function to setup streamlit titles etc configurations
+        """
+        st.set_page_config(page_title="OpenChat",)
+
+        st.header("OpenChat")
+        ## TODO This should recieve values when called, not have them hard coded
+        ## Does this function even belong here? is it part of the chatbot, or part of the overall wrapper?
+        st.sidebar.title(sidebar_title)
+        st.sidebar.write(sidebar_text)
+        return
+setup_streamlit() 
 
 if __name__ == "__main__":
     obj = ContextChatbot()
