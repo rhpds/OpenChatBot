9e02bb9bed395594554e4a46989268950dba78d1
Tony Kay <tony.g.kay@gmail.com>
2024-05-14 19:55:30 -0600

    Move settings from Sha/yaml to python/native

--- libs/ui.py
+++ libs/ui.py
@@ -1,11 +1,13 @@
 import config as cfg
 import chainlit as cl
+from chainlit.input_widget import Select, Switch, Slider
 from libs.ai import setup_chain
 from langchain.schema.runnable.config import RunnableConfig
 
 
 @cl.on_chat_start
-def on_chat_start():
+async def on_chat_start():
+    await setup_chat_settings()
     chain = setup_chain()                                   # Setup the chain
     cl.user_session.set("chain", chain)                     # Save the chain to the chainlit user session
 
@@ -22,3 +24,38 @@ async def on_message(message: cl.Message):
         await msg.stream_token(chunk)
 
     await msg.send()
+
+async def setup_chat_settings():
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="Ollama - Model",
+                values=[
+                    "mistral",
+                    "llama2"
+                ],
+                initial_index=0,
+            ),
+            Select(
+                id="Chains",
+                label="Chains",
+                values=["chatbot_llm", "chabot_rag"],
+                initial_index=1,
+            ),
+            Switch(
+                id="Streaming",
+                label="Stream Tokens",
+                initial=True,
+            ),
+            Slider(
+                id="Temperature",
+                label="Ollama - Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+        ]
+    ).send()
+    # await setup_agent(settings)
